8d0e8eee69cb29c7286f9ab35c383431e44ded40
Electron Sky Space <namsosoluciones@gmail.com>
2018-11-21 23:17:15 -0600

    Algoritmo de selección completo y carga de DB

--- js/engine.js
+++ js/engine.js
@@ -1,18 +1,23 @@
-var container, stats;
-var camera, controls, scene, renderer;
-var objects = [];
-var cubeSize=50;
+var camera, controls, scene, renderer, raycaster;
+var objects=[], newpos=[], db=[];
+var cubeSize=50, complete=0, topA=50, cont=0, topB=50, typeS=1;
 var cc=document.getElementById("canvasElement");
+var mouse = new THREE.Vector2();
 
 init();
 animate();
+setEvents();
+loadDB();
 
 function init() {
 	//Creando la camara
 	camera = new THREE.PerspectiveCamera( 70, cc.clientWidth / cc.clientHeight, 1, 1000 );
 	camera.position.set( cubeSize*3, cubeSize*3, cubeSize*3 );
+	getCamera();
+	getRotation();
 	
 	//Creando el render
+	raycaster = new THREE.Raycaster();
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setPixelRatio( cc.devicePixelRatio );
 	renderer.setSize( cc.clientWidth, cc.clientHeight );
@@ -20,7 +25,7 @@ function init() {
 	renderer.shadowMap.type = THREE.PCFShadowMap;
 	
 	//Creando los controles
-	controls = new THREE.TrackballControls( camera );
+	controls = new THREE.TrackballControls( camera, cc );
 	controls.rotateSpeed = 1.0;
 	controls.zoomSpeed = 1.2;
 	controls.panSpeed = 0.8;
@@ -36,6 +41,8 @@ function init() {
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0xf0f0f0 );
 	scene.add( new THREE.AmbientLight( 0x505050 ) );
+	pickingTexture = new THREE.WebGLRenderTarget( cc.clientWidth, cc.clientHeight );
+	pickingTexture.texture.minFilter = THREE.LinearFilter;
 	var light = new THREE.SpotLight( 0xffffff, 1.5 );
 	light.position.set( 0, 500, 2000 );
 	light.angle = Math.PI / 9;
@@ -46,6 +53,7 @@ function init() {
 	light.shadow.mapSize.height = 1024;
 	scene.add( light );
 	
+	var cubeAux=0;
 	//Creando el cubo
 	for(var a=0;a<3;a++){//Z
 		for(var b=0;b<3;b++){//Y
@@ -73,8 +81,12 @@ function init() {
 				object.castShadow = true;
 				object.receiveShadow = true;
 				
+				object.name="cube"+(cubeAux);
+				
 				scene.add( object );
 				objects.push( object );
+				
+				cubeAux++;
 			}
 		}
 	}
@@ -82,18 +94,28 @@ function init() {
 	//Estableciendo el render
 	cc.innerHTML ="";
 	cc.appendChild( renderer.domElement );
+}
+
+function setEvents(){
+	document.getElementById("close").addEventListener("click", showModal, false);
+	document.getElementById("perpectiveMX").addEventListener("input", setCamera, false);
+	document.getElementById("perpectiveMY").addEventListener("input", setCamera, false);
+	document.getElementById("perpectiveMZ").addEventListener("input", setCamera, false);
+	document.getElementById("rotateMX").addEventListener("input", setRotation, false);
+	document.getElementById("rotateMY").addEventListener("input", setRotation, false);
+	document.getElementById("rotateMZ").addEventListener("input", setRotation, false);
+	/*document.getElementById("rotateCMX").addEventListener("input", setRotationC, false);
+	document.getElementById("rotateCMY").addEventListener("input", setRotationC, false);
+	document.getElementById("rotateCMZ").addEventListener("input", setRotationC, false);*/
 	
-	//Configurando los eventos del control drag del mouse
-	var dragControls = new THREE.DragControls( objects, camera, renderer.domElement );
-	dragControls.addEventListener( 'dragstart', function () {
-		controls.enabled = false;
-	} );
-	dragControls.addEventListener( 'dragend', function () {
-		controls.enabled = true;
+	renderer.domElement.addEventListener("click", onclick, false);
+	controls.addEventListener( 'change', function (){
+		getRotation();
+		getCamera();
 	} );
 	
 	//Sobrecargando el evento de tamaño
-	window.addEventListener( 'resize', onWindowResize, false );
+	window.addEventListener('resize', onWindowResize, false);
 }
 
 function onWindowResize() {
@@ -105,20 +127,92 @@ function onWindowResize() {
 function animate() {
 	requestAnimationFrame( animate );
 	if(document.getElementById("rotateX").checked)
-		scene.rotation.x += 0.5*Math.PI/180;
+		scene.rotation.x += document.getElementById("myRange").value*Math.PI/180;
 
 	if(document.getElementById("rotateY").checked)
-		scene.rotation.y += 0.5*Math.PI/180;
+		scene.rotation.y += document.getElementById("myRange").value*Math.PI/180;
 
 	if(document.getElementById("rotateZ").checked)
-		scene.rotation.z += 0.5*Math.PI/180;
+		scene.rotation.z += document.getElementById("myRange").value*Math.PI/180;
 		
+	getRotation();	
+	
 	controls.update();
 	renderer.render( scene, camera );
 }
 
-var newpos=[];
-var complete=0, topA=50;
+function onclick(e) {
+	var rect = cc.getBoundingClientRect();
+	mouse.x = ( (e.clientX - rect.left ) / rect.width ) * 2 - 1;
+	mouse.y = - ( ( e.clientY - rect.top ) / rect.height ) * 2 + 1;
+	raycaster.setFromCamera( mouse, camera );
+	var intersects = raycaster.intersectObjects( objects );
+	if ( intersects.length > 0 ) {
+		var object = intersects[ 0 ].object;
+		getText(object.name);
+		document.getElementById("msg").style.display="block";
+		console.log(object.name + " seleccionado!");
+	}
+}
+
+function setCamera(){
+	var x=document.getElementById("perpectiveMX").value;
+	var y=document.getElementById("perpectiveMY").value;
+	var z=document.getElementById("perpectiveMZ").value;
+	x=isNaN(x)? 0 : parseFloat(x);
+	y=isNaN(y)? 0 : parseFloat(y);
+	z=isNaN(z)? 0 : parseFloat(z);
+	camera.position.set( x, y, z);
+}
+
+function setRotation(){
+	var x=document.getElementById("rotateMX").value;
+	var y=document.getElementById("rotateMY").value;
+	var z=document.getElementById("rotateMZ").value;
+	x=isNaN(x)? 0 : parseFloat(x);
+	y=isNaN(y)? 0 : parseFloat(y);
+	z=isNaN(z)? 0 : parseFloat(z);
+	scene.rotation.set( x, y, z);
+}
+
+function setRotationC(){
+	var x=document.getElementById("rotateCMX").value;
+	var y=document.getElementById("rotateCMY").value;
+	var z=document.getElementById("rotateCMZ").value;
+	x=isNaN(x)? 0 : parseFloat(x);
+	y=isNaN(y)? 0 : parseFloat(y);
+	z=isNaN(z)? 0 : parseFloat(z);
+	camera.rotation.set( x, y, z);
+}
+
+function getCamera(){
+	document.getElementById("perpectiveMX").value=roundR(camera.position.x);
+	document.getElementById("perpectiveMY").value=roundR(camera.position.y);
+	document.getElementById("perpectiveMZ").value=roundR(camera.position.z);
+}
+
+function getRotation(){
+	if(scene){
+		var x=scene.rotation.x;
+		var y=scene.rotation.y;
+		var z=scene.rotation.z;
+		x=isNaN(x)? 0 : x;
+		y=isNaN(y)? 0 : y;
+		z=isNaN(z)? 0 : z;
+		document.getElementById("rotateMX").value=roundR(x);
+		document.getElementById("rotateMY").value=roundR(y);
+		document.getElementById("rotateMZ").value=roundR(z);
+	}
+	if(camera){
+		document.getElementById("rotateCMX").value=roundR(camera.rotation.x);
+		document.getElementById("rotateCMY").value=roundR(camera.rotation.y);
+		document.getElementById("rotateCMZ").value=roundR(camera.rotation.z);
+	}	
+}
+
+function roundR(n){
+	return Math.round(n*1000000)/1000000;
+}
 
 function explodeC(){
 	complete=0;
@@ -179,7 +273,6 @@ function restorePosition(){
 	renderer.render( scene, camera );
 }
 
-var cont=0, topB=50, typeS=1;
 function slice(type){
 	restorePosition();
 	cont=0;
@@ -228,21 +321,40 @@ function moveSlice(){
 	controls.update();
 	renderer.render( scene, camera );
 	cont++;
+	getCamera();
+	getRotation();
 	if(cont<topB)
 		requestAnimationFrame( moveSlice );
-	else{
-		/*switch(typeS){
-			case 1://Critical Information Characteristics
-				scene.rotation.y += 40*Math.PI/180;
-				break;
-			case 2://Information States
-				scene.rotation.x += 1*Math.PI/180;
-				scene.rotation.y += 1*Math.PI/180;
-				break;
-			case 3://Security Measures
-				scene.rotation.z += 1*Math.PI/180;
-				break;
+}
+
+function showModal(){
+	document.getElementById("msg").style.display=document.getElementById("msg").style.display=="none"? "block" : "none";
+}
+
+function getText(name){
+	for(var i=0;i<db.length;i++){
+		if(db[i][0]==name){
+			document.getElementById("text").innerHTML=db[i][1];
+			break;
 		}
-		*/
+	}	 
+}
+
+function loadDB(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			processData(this.responseText);
+		}
+	};
+	xhttp.open("GET", "data.db", true);
+	xhttp.send();
+}
+
+function processData(d){
+	var lines = d.split(/\r\n|\n/);
+	for (var i=0; i<lines.length; i++) {
+		var data = lines[i].split('=>');
+		db[i]=data;
 	}
 }
